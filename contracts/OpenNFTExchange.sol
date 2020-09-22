d7763860e954032d3da51aac925d09d2632277b3
Andy <admazzola@gmail.com>
2020-09-22 15:03:13 -0400

    update contract

--- contracts/OpenNFTExchange.sol
+++ contracts/OpenNFTExchange.sol
@@ -252,10 +252,49 @@ contract StandardToken is Token {
 contract OpenNFTExchange is SafeMath {
 
 
+//https://github.com/larvalabs/cryptopunks/blob/master/contracts/CryptoPunksMarket.sol
 
-  mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
-  mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
-  mapping (address => mapping (bytes32 => uint)) public orderFills; //mapping of user accounts to mapping of order hashes to uints (amount of order that has been filled)
+
+
+
+    struct Offer {
+        bool isForSale;
+
+        address tokenAddress;
+        uint tokenId;
+
+        address sellerAddress;
+
+        address currencyTokenAddress;
+        uint minValue;          // in the curency
+
+        address onlySellTo;     // specify to sell only to a specific person
+    }
+
+    struct Bid {
+        bool hasBid;
+
+        address tokenAddress;
+        uint tokenId;
+
+        address bidderAddress;
+
+        address currencyTokenAddress;
+        uint value;
+    }
+
+
+    mapping (address => mapping (uint => address)) public itemsInEscrow;  //itemsInEscrow[nftAddress][indexOfToken] => ownerAddress
+
+
+    mapping (address => mapping (uint => Offer)) public itemsOfferedForSale;   //itemsOfferedForSale[nftAddress][indexOfToken] => Offer
+
+    // A record of the highest item bid
+    mapping (address=> mapping(uint => Bid)) public itemBids;   //itemBidsOfferedForSale[nftAddress][indexOfToken] => Bid
+
+  //mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
+  //mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
+  //mapping (address => mapping (bytes32 => uint)) public orderFills; //mapping of user accounts to mapping of order hashes to uints (amount of order that has been filled)
 
   event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
   event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
@@ -267,15 +306,15 @@ contract OpenNFTExchange is SafeMath {
 
   }
 
-  //Do not allow ETH to enter
+  //Do not allow Currency to enter
   function() payable {
     throw;
   }
 
 
-  //call this using ApproveAndCall
-  //allows for interacting with ether directly as token[0]
-  function depositToken(address token, uint amount) {
+  //deposit an NFT token into the exchange's escrow - requires an Approve first
+
+  function depositNFT(address _nftContractAddress, uint _itemId) {
     //remember to call Token(address).approve(this, amount) or this contract will not be able to do the transfer on your behalf.
     if (token==0) throw;
     if (!Token(token).transferFrom(msg.sender, this, amount)) throw;
@@ -283,7 +322,7 @@ contract OpenNFTExchange is SafeMath {
     Deposit(token, msg.sender, amount, tokens[token][msg.sender]);
   }
 
-  function withdrawToken(address token, uint amount) {
+  function withdrawNFT(address _nftContractAddress, uint _itemId) {
     if (token==0) throw;
     if (tokens[token][msg.sender] < amount) throw;
     tokens[token][msg.sender] = safeSub(tokens[token][msg.sender], amount);
@@ -291,9 +330,13 @@ contract OpenNFTExchange is SafeMath {
     Withdraw(token, msg.sender, amount, tokens[token][msg.sender]);
   }
 
-  function balanceOf(address token, address user) constant returns (uint) {
+  function getOwnerOfNFT(address _nftContractAddress, address _itemId) constant returns (uint) {
     return tokens[token][user];
   }
+
+
+
+
 
   function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {
     bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
@@ -323,30 +366,6 @@ contract OpenNFTExchange is SafeMath {
     tokens[tokenGive][msg.sender] = safeAdd(tokens[tokenGive][msg.sender], safeMul(amountGive, amount) / amountGet);
   }
 
-/*  function testTrade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount, address sender) constant returns(bool) {
-    if (!(
-      tokens[tokenGet][sender] >= amount &&
-      availableVolume(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, user, v, r, s) >= amount
-    )) return false;
-    return true;
-  }*/
-
-  function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {
-    bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
-    if (!(
-      (orders[user][hash] || ecrecover(sha3("\x19Ethereum Signed Message:\n32", hash),v,r,s) == user) &&
-      block.number <= expires
-    )) return 0;
-    uint available1 = safeSub(amountGet, orderFills[user][hash]);
-    uint available2 = safeMul(tokens[tokenGive][user], amountGet) / amountGive;
-    if (available1<available2) return available1;
-    return available2;
-  }
-
-  function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {
-    bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
-    return orderFills[user][hash];
-  }
 
   function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {
     bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
@@ -354,4 +373,8 @@ contract OpenNFTExchange is SafeMath {
     orderFills[msg.sender][hash] = amountGet;
     Cancel(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, msg.sender, v, r, s);
   }
+
+
+
+
 }
